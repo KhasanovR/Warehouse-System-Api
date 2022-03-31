3b8cdfb830f8891b0c841fddc2c3ea99ae2b102d
Rakhmatjon Khasanov <r.khasanov97@gmail.com>
2022-03-31 16:31:05 +0500

    add request payload in testing

--- warehouse/api/core/tests/core_tests.py
+++ warehouse/api/core/tests/core_tests.py
@@ -6,7 +6,18 @@ from rest_framework.reverse import reverse
 @pytest.mark.management
 @pytest.mark.receipt
 @pytest.mark.receipt_accept
-def test_process_works(api_client):
+def test_process_works(api_client, product):
+    data = {
+        'request': [
+            {
+                'product': {
+                    'id': product.id,
+                },
+                'quantity': 2,
+            }
+        ]
+    }
     response = api_client.post(reverse('v1:pharmacy_cloud.management:results-process'),
+                               data,
                                content_type='application/json')
     assert response.status_code == 200
